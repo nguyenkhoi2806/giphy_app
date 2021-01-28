1bbcd2a94ab499e7d5d8a4004fca46a86ebdb2c6
nguyenkhoi2806 <lenguyenkhoi2806@gmail.com>
2021-01-28 10:19:47 +0700

    update code

--- src/components/ListImage/index.js
+++ src/components/ListImage/index.js
@@ -1,87 +1,69 @@
 import Image from "../Image";
-import { isTablet, isMobileOnly } from "react-device-detect";
+import { isTablet, isMobileOnly, isBrowser } from "react-device-detect";
 
-const ItemImage = ({ item }) => (
-  <div className="col-xl-3 col-md-4 col-6 p-1 ">
-    <div className="card card-custom">
-      <div className="card-body">
-        <Image
-          src={item.images.downsized.url}
-          className="card-img-top"
-          alt={item.id}
-          preview={item.images.original.url}
-        />
-      </div>
-      <div className="user">
-        {item.user && (
-          <>
-            <img
-              src={item.user.avatar_url}
-              width="10px"
-              alt={item.user.display_name}
-            />
-            <span className="user-name">{item.user.display_name}</span>
-          </>
-        )}
+const ItemImage = ({ item }) => {
+  if (!item) return null;
+  return (
+    <div className="col-xl-3 col-md-4 col-6">
+      <div className="card card-custom m-0">
+        <div className="card-body">
+          <Image
+            src={item.images.downsized.url}
+            className="card-img-top"
+            alt={item.id}
+            preview={item.images.original.url}
+          />
+        </div>
+        <div className="user">
+          {item.user && (
+            <>
+              <img
+                src={item.user.avatar_url}
+                width="10px"
+                alt={item.user.display_name}
+              />
+              <span className="user-name">{item.user.display_name}</span>
+            </>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 const ListImage = (props) => {
   const imageLists = props.imageLists;
   if (!imageLists) return null;
 
   let newImageList = [];
+  /*  Add items to show in one row follow by device detect */
   imageLists.forEach((item, index) => {
-    if (index % 4 === 0 ) {
+    if (index % 4 === 0 && isBrowser) {
       newImageList.push([
         imageLists[index],
-        imageLists[index + 1],
-        imageLists[index + 2],
-        imageLists[index + 3],
+        imageLists[index + 1] && imageLists[index + 1],
+        imageLists[index + 2] && imageLists[index + 2],
+        imageLists[index + 3] && imageLists[index + 3],
       ]);
-    } else if (index % 3 === 0 && isTablet && imageLists[index + 2]) {
-        console.log('ok')
+    } else if (index % 3 === 0 && isTablet) {
       newImageList.push([
         imageLists[index],
-        imageLists[index + 1],
-        imageLists[index + 2],
+        imageLists[index + 1] && imageLists[index + 1],
+        imageLists[index + 2] && imageLists[index + 2],
       ]);
-    } else if(index % 2 === 0 && isMobileOnly) {
-        newImageList.push([
-          imageLists[index],
-          imageLists[index + 1],
-        ]);
+    } else if (index % 2 === 0 && isMobileOnly) {
+      newImageList.push([
+        imageLists[index],
+        imageLists[index + 1] && imageLists[index + 1]]);
     }
   });
 
-  console.log(newImageList)
-  return newImageList.map((item, index) => {
-    if (isTablet)
-      return (
-        <div className="row card-deck" key={index}>
-          <ItemImage key={index} item={item[0]} />
-          <ItemImage key={index} item={item[1]} />
-          <ItemImage key={index} item={item[2]} />
-        </div>
-      );
-    if(isMobileOnly)
-        return (
-        <div className="row card-deck" key={index}>
-            <ItemImage key={index} item={item[0]} />
-            <ItemImage key={index} item={item[1]} />
-        </div>
-        );
-
-    return (
-        <div className="row card-deck" key={index}>
-          <ItemImage key={index} item={item[0]} />
-          <ItemImage key={index} item={item[1]} />
-          <ItemImage key={index} item={item[2]} />
-          <ItemImage key={index} item={item[3]} />
-        </div>
-    );
-  });
+  return newImageList.map((items, index) => (
+    <div className="row card-deck mb-3" key={index}>
+      {items.map((item, keyItem) => (
+        <ItemImage key={keyItem} item={item} />
+      ))}
+    </div>
+  ));
 };
 
 export default ListImage;
